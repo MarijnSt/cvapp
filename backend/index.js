928aa70b41f7290db9d025a16766b01719ff1e9d
MarijnSt <marijn.stammeleer@gmail.com>
2019-08-08 11:10:54 +0200

    form works in prod env

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,6 @@ const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
 const app = express()
 
-const clientID = "914935273305-rf37n2gnb7bhavkauqudp051nkfj4ufk.apps.googleusercontent.com"
-const cSecret = "LpvxkYEf29Ddo6HmfUDQ5dDb"
-const refreshT = "1/7FfOGD5CqDzRWiLBJhxwXVbFFhA0Fu7nDorYYlsa5fI"
-const accessT = "ya29.GltWBx0RzFa38WLj_xP-nGzxkSpnFUdED2xqr5CmNi8QzPYBZDAO_35cpg00eWA_BRR8MIGe37WUaZ3HsKSLN65p1qWGh9BWNPtH_waxZgvYxtAN6t2Q2ciEBPhk"
-
 app.use(express.static('build'))
 
 app.use(bodyParser.urlencoded({extended:false}))
@@ -27,37 +22,35 @@ app.post('/send', (req, res) => {
         <p>${req.body.message}</p>
     `
     console.log(output)
+    
     res.json(req.body)
 
-    // //transporter
-    // let transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         type: 'oauth2',
-    //         user: 'cvmarijn@gmail.com',
-    //         clientId: clientID,
-    //         clientSecret: cSecret,
-    //         refreshToken: refreshT,
-    //         accessToken: accessT
-    //     }
-    // })
+    //transporter
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.ACC, //replace in dev mode
+            pass: process.env.PASS //replace in dev mode
+        }
+    })
 
-    // //mailoptions
-    // let mailOptions = {
-    //     from: '"CV Site" <cvmarijn@gmail.com>',
-    //     to: 'marijn.stammeleer@gmail.com',
-    //     subject: 'New message on cv site',
-    //     html: output
-    // }
+    //mailoptions
+    let mailOptions = {
+        from: '"CV Site" <cvmarijn@gmail.com>',
+        to: 'marijn.stammeleer@gmail.com',
+        subject: 'New message from cv site',
+        html: output
+    }
 
-    // // send mail
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //     if (error) {
-    //         return console.log(error)
-    //     }
-    //     console.log("Message sent: %s", info.messageId);
-    //     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // })
+    // send mail
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log("Message sent: %s", info.messageId);
+    })
 })
 
 const PORT = process.env.PORT || 3001
